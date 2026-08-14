30093b44a787cb06ce5a0449461de68bd5e31f49
Archangel <jesse@archangel.wtf>
2026-08-14 22:32:29 +0200

    Reduce allocations when the client starts up

--- Libraries/SPTushonka.Server.Core/Callbacks/DataCallbacks.cs
+++ Libraries/SPTushonka.Server.Core/Callbacks/DataCallbacks.cs
@@ -1,4 +1,4 @@
-using SPTarkov.DI.Annotations;
+﻿using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.Controllers;
 using SPTarkov.Server.Core.Models.Common;
 using SPTarkov.Server.Core.Models.Eft.Common;
